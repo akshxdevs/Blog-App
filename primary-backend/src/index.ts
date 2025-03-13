83b1721a09eb83dfae615f54dff23d5aacecf97c
akshxdevs <akshxdevs@gmail.com>
2025-03-13 19:42:32 +0530

    added some models in prisma schema

---
+++ primary-backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import { PORT } from "./config";
+import { userRouter } from "./routes/user";
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1/user",userRouter)
+
+app.listen(PORT,()=>{
+    console.log("Server running on port 3000");
+    
+})
