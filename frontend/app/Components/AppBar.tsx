94176a26486208d116e2bde005c004dfcdaaeb2e
Akash R <84657690+akshlyfy03@users.noreply.github.com>
2025-03-14 19:38:26 +0530

    added navbar

--- frontend/app/Components/AppBar.tsx
+++ frontend/app/Components/AppBar.tsx
@@ -1,9 +1,9 @@
 export const AppBar = () =>  {
     return <div>
-        <div className="fixed w-full">
+        <div className="fixed left-20 w-full">
             <div className="flex justify-between p-4 border-b border-gray-800">
                 <div>
-                    <h1 className="text-2xl pl-24 py-3 font-bold">Home</h1>  
+                    <h1 className="text-2xl pl- py-3 font-bold">Home</h1>  
                 </div>
                 <div className="flex w-2/5 border my-2 border-gray-800 focus-within:border-blue-500 transition-all duration-200 h-10 rounded-3xl bg-[#191919]">
                     <button className="pl-5">
@@ -13,7 +13,7 @@ export const AppBar = () =>  {
                     </button>
                     <input type="text" placeholder="Search Substack" className="bg-[#191919] w-full pl-3 rounded-3xl outline-none"/>
                 </div>
-                <div className="flex py-2">
+                <div className="flex px-20 py-2">
                     <div className="pr-5">
                         <button className="bg-gray-800 px-5 py-2 rounded-lg font-semibold">Sign in</button>
                     </div>
